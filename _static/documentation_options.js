3841b83f6470186877f0f40b499439b01e383a3c
jan-david-fischbach <jan-david-fischbach@users.noreply.github.com>
2026-02-25 16:33:02 +0000

    deploy: d24f1430cc8a28ec49e7bf04f8513a6bdfd5b5c1

---
+++ _static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '0.1.dev286+gd24f1430c',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
